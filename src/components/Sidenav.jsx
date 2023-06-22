6f940ed722db032fe523215d8b61bbd97f4533da
Christian <christiantegene@gmail.com>
2023-06-22 11:40:27 -0400

    Configured Vite React App with Tailwind

---
+++ src/components/Sidenav.jsx
@@ -0,0 +1,17 @@
+import React, {useState} from 'react'
+import {AiOutlineMenu} from 'react-icons/ai'
+
+const Sidenav = () => {
+
+    const [nav, setNav] = useState(false)
+    const handleNav = () => {
+        setNav(!nav);
+    }
+    return (
+        <div>
+            <AiOutlineMenu className='absolute top-4 '/>
+        </div>
+    )
+}
+
+export default Sidenav
