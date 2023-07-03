865daf752b96b0896e25299337535a4b88f40908
Christian <christiantegene@gmail.com>
2023-07-03 13:27:59 -0400

    About Me section completed

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 import MyLinks from './MyLinks'
 import { Carousel } from "@material-tailwind/react";
 import headshotImg from '../assets/headshot.jpg'
-
+import stamfordbridgeImg from '../assets/stamfordbridge.jpg'
+import stamfordbridgeImg2 from '../assets/stamfordbridge2.jpg'
+import eiffeltowerImg from '../assets/eiffeltower.jpg'
+import pinchotlakeImg from '../assets/pinchotlake.jpg'
+import skydivingImg from '../assets/skydiving.jpg'
+import scubadivingImg from '../assets/scubadiving.jpg'
 
 
 const AboutMe = () => {
@@ -13,44 +18,53 @@ const AboutMe = () => {
                 <h1 className='text-center pt-10 pb-5 sm:text-5xl text-4xl font-bold text-gray-800'>About Me</h1>
                 <div className='flex flex-col items-center md:flex-row'>
                     <img className='rounded-full w-57 h-48' src={headshotImg} alt="me" />
-                    <div>
-                        <p className='h-full w-full flex flex-col justify-center'>
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit aliquid suscipit, praesentium recusandae eligendi error quod odit harum temporibus facere tempora obcaecati exercitationem laudantium blanditiis nulla voluptate consequuntur alias itaque.
-                        Autem saepe alias repudiandae accusantium odio est excepturi aliquam inventore quam accusamus. Eos accusantium iste explicabo beatae veritatis, fugiat facere sapiente atque nihil eveniet reprehenderit porro optio fuga laborum ipsum.
-                        Sapiente rem harum temporibus dicta, consequatur neque porro accusantium deleniti fuga repellendus voluptates at? Itaque accusamus esse ullam amet. Magnam dignissimos aliquam ducimus eius recusandae sequi sed, libero incidunt ullam.
-                        Nulla, animi? Iusto ullam rem, explicabo impedit rerum eaque ipsam soluta deleniti odit aut quisquam cum est? Eveniet laudantium corporis, tempore cupiditate ipsum quidem. Accusantium, quia? Facilis assumenda quibusdam perferendis.
+                    <div className='pt-5'>
+                        <p className='h-full w-full flex flex-col justify-center pl-10 pb-5'>
+                        Welcome! I'm Christian Tegene, a 25 year old Full-Stack developer, former medical student, and passionate problem solver embarking on an exciting career transition into the world of tech. I graduated from the University of Pittsburgh where I studied Biology and Chemistry with the goal of pursuing medicine. However, as I progressed through medical school, I realized that my true calling lies in the realm of technology. Through a combination of self-guided and structured learning and a deep exploration of computer science and programming, I have found immense joy and fulfillment in building software and solving complex technical problems. With a strong foundation in critical thinking, decision-making, and attention to detail, coupled with my background in the natural sciences, I bring a unique perspective to the field of software engineering.
+                        </p>
+                        <p className='h-full w-full flex flex-col justify-center pl-10'>
+                        Outside of the tech world, I have a diverse range of hobbies that keep me inspired and balanced. I enjoy playing soccer, piano, and chess, which allow me to channel my competitive spirit and exercise my strategic thinking skills. These activities have not only taught me discipline and perseverance but have also fostered my ability to work as part of a team and think critically in high-pressure situations. I also enjoy traveling the world. Check out some of my recent adventures below and join me as I set out on my next thrilling adventure of breaking into tech.
                         </p>
                     </div>
 
                 </div>
-                <Carousel className="rounded-xl max-w-[500px] m-auto">
+                <Carousel loop={true} autoplay={true} className="rounded-xl max-w-[500px] m-auto pt-10">
                     <img
-                        src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
-                        alt=""
+                        src={pinchotlakeImg}
+                        alt="pinchot lake"
                         className="h-full w-full object-cover"
                     />
+                    <div>
+                        <img
+                            src={stamfordbridgeImg}
+                            alt="stamford bridge"
+                            className="h-full w-full object-cover"
+                        />
+                        <img
+                            src={stamfordbridgeImg2}
+                            alt="skydiving"
+                            className="h-full w-full object-cover"
+                        />
+                    </div>
+                    <div>
+                        <img
+                            src={skydivingImg}
+                            alt="skydiving"
+                            className="h-full w-full object-cover pt-3"
+                        />
+                        <img
+                            src={scubadivingImg}
+                            alt="scuba diving"
+                            className="h-full w-full object-cover"
+                        />
+                    </div>
                     <img
-                        src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
-                        alt=""
+                        src={eiffeltowerImg}
+                        alt="eiffel tower"
                         className="h-full w-full object-cover"
                     />
-                    <img
-                        src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
-                        alt=""
-                        className="h-full w-full object-cover"
-                    />
-                    {/* <img
-                        src="https://www.spaza.se/wp-content/uploads/woocommerce-placeholder.png"
-                        alt=""
-                        className="h-full w-full object-cover"
-                    /> */}
                 </Carousel>
-                {/* <div className='flex justify-around'>
-                    <MyLinks />
-                </div> */}
             </div>
-
-
         </div>
     )
 }
