d8c85cbc2093886625f00bc8cf4847486a771ad5
Christian <christiantegene@gmail.com>
2023-07-04 13:17:51 -0400

    Fixed logo in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import MyLinks from './MyLinks'
+import Logo from '../assets/logo.png'
+
 
 const Footer = () => {
     return (
         <div className="bg-[#273036] flex justify-around">
-            <img src="/logo.png" alt="/" className='w-20 h-14'/>
-            <p className='text-white flex items-end'>&copy;2023  Christian Tegene</p>
             <MyLinks />
+            <img src={Logo} alt="/" className='w-20 h-14'/>
+
+
+
+            <p className='text-white flex items-end'>&copy;2023  Christian Tegene</p>
         </div>
     )
 }
