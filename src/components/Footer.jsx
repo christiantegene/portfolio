08b8da62fa0415fc69f9e0d290ebcd7e0c737ef3
Christian <christiantegene@gmail.com>
2023-07-07 11:07:56 -0400

    Fixed footer and greeting on mobile

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <div className="bg-[#273036] flex justify-around">
             <img src="/logo.png" alt="/" className='w-20 h-14'/>
-            <p className='text-white flex items-end'>&copy;2023  Christian Tegene</p>
+            <p className='text-white flex items-end'>&copy; 2023  Christian Tegene</p>
             <MyLinks />
         </div>
     )
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
             <img className='w-full h-screen object-cover' src="https://images.unsplash.com/photo-1491895200222-0fc4a4c35e18?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80" alt="light-bg" />
             <div className='w-full h-screen absolute top-0 left-0 bg-white/30'>
                 <div className='max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-center sm:items-center items-center'>
-                    <h1 className='sm:text-5xl text-4xl font-bold text-gray-800'>Hello, my name is Christian.</h1>
+                    <h1 className='sm:text-5xl text-3xl font-bold text-gray-800'>Hello, my name is Christian.</h1>
                     <h2 className='sm:text-3xl text-2xl pt-4 text-gray-800'>I'm a Full-Stack developer.</h2>
                     <MyLinks />
                 </div>
