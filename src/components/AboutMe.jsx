277d1444cf21bf3284ecb2d416e4c78d116c3442
Christian <christiantegene@gmail.com>
2023-08-09 21:18:20 -0400

    Fixed spacing on about paragraphs on mobile and web

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -19,10 +19,10 @@ const AboutMe = () => {
                 <div className='flex flex-col items-center md:flex-row'>
                     <img className='rounded-full w-57 h-48' src={headshotImg} alt="me" />
                     <div className="pt-3 pb-2 border border-black rounded-md">
-                        <p className='h-full w-full flex flex-col justify-center pl-10 pb-5'>
+                        <p className='h-full w-full flex flex-col p-3'>
                         Welcome! I'm Christian Tegene, a 25 year old Full-Stack developer, former medical student, and passionate problem solver embarking on an exciting career transition into the world of tech. I graduated from the University of Pittsburgh where I studied Biology and Chemistry with the goal of pursuing medicine. However, as I progressed through medical school, I realized that my true calling lies in the realm of technology. Through a combination of self-guided and structured learning and a deep exploration of computer science and programming, I have found immense joy and fulfillment in building software and solving complex technical problems. With a strong foundation in critical thinking, decision-making, and attention to detail, coupled with my background in the natural sciences, I bring a unique perspective to the field of software engineering.
                         </p>
-                        <p className='h-full w-full flex flex-col justify-center pl-10'>
+                        <p className='h-full w-full flex flex-col p-3'>
                         Outside of the tech world, I have a diverse range of hobbies that keep me inspired and balanced. I enjoy playing soccer, piano, and chess, which allow me to channel my competitive spirit and exercise my strategic thinking skills. These activities have not only taught me discipline and perseverance but have also fostered my ability to work as part of a team and think critically in high-pressure situations. I also enjoy traveling the world. Check out some of my recent adventures below and join me as I set out on my next thrilling adventure of breaking into tech.
                         </p>
                     </div>
