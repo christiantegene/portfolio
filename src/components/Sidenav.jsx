cd762ca433f395676b31a7cc3f7577116770ccaa
Christian <christiantegene@gmail.com>
2023-08-27 20:07:44 -0400

    Fixed Fill Opacity Console Error

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,5 +1,7 @@
 import React, {useState} from 'react'
-import {AiOutlineHome, AiOutlineMenu, AiOutlineProject, AiOutlineMail} from 'react-icons/ai'
+import {AiOutlineHome, AiOutlineMenu, AiOutlineProject, AiOutlineMail } from 'react-icons/ai'
+import {RxCross2} from 'react-icons/Rx'
+
 import {BsPerson} from 'react-icons/bs'
 import {GrProjects} from 'react-icons/gr'
 import MyLinks from './MyLinks'
