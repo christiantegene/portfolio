d1258338f49c1810eb067884c36d6a92612851a1
Christian <christiantegene@gmail.com>
2023-08-29 21:37:36 -0400

    Fixed Footer content justification

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import Logo from '../assets/logo.png'
 
 const Footer = () => {
     return (
-        <div className="bg-black flex justify-between sm:justify-around pt-5 pb-0">
+        <div className="bg-black flex justify-around pt-5 pb-0">
             <a href="#home"><img src={Logo} alt="/" className='w-24 h-16'/></a>
             <div>
                 <MyLinks githubColor="white"/>
