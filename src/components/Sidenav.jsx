f5a1e37f1c69d1b3907cab4b4ca7dfb6709d1df2
christiantegene <christiantegene@gmail.com>
2024-05-30 17:34:47 -0400

    Extracted navBar classNames into variables

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -12,29 +12,31 @@ const Sidenav = () => {
         setNav(!nav);
     };
 
+    const mobileNavBarButton = 'w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'
+    const navBarButton = 'rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'
     return (
         <div>
             <AiOutlineMenu size={30} onClick={handleNav} className='fixed top-4 right-4 z-[99] md:hidden'/>
             {
                 nav ? (
                     <div className='fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20'>
-                        <a onClick={handleNav} href="#home" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                        <a onClick={handleNav} href="#home" className={mobileNavBarButton}>
                             <AiOutlineHome size={20} />
                             <span className='pl-4'>Home</span>
                         </a>
-                        <a onClick={handleNav} href="#about" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                        <a onClick={handleNav} href="#about" className={mobileNavBarButton}>
                             <BsPerson size={20} />
                             <span className='pl-4'>About Me</span>
                         </a>
-                        <a onClick={handleNav} href="#skills" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                        <a onClick={handleNav} href="#skills" className={mobileNavBarButton}>
                             <AiOutlineProject size={20} className='rotate-180' />
                             <span className='pl-4'>My Skills</span>
                         </a>
-                        <a onClick={handleNav} href="#portfolio" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                        <a onClick={handleNav} href="#portfolio" className={mobileNavBarButton}>
                             <GrProjects size={20} />
                             <span className='pl-4'>Portfolio</span>
                         </a>
-                        <a onClick={handleNav} href="#contact" className=' w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                        <a onClick={handleNav} href="#contact" className={mobileNavBarButton}>
                             <AiOutlineMail size={20} />
                             <span className='pl-4'>Contact Me</span>
                         </a>
@@ -48,45 +50,30 @@ const Sidenav = () => {
             <div className='md:block hidden fixed top-[25%] z-10'>
                 <div className='flex flex-col'>
                     <Tooltip content="Home" placement="right">
-                        <a href="#home" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
-                                    <AiOutlineHome size={20} />
+                        <a href="#home" className={navBarButton}>
+                            <AiOutlineHome size={20} />
                         </a>
                     </Tooltip>
                     <Tooltip content="About Me" placement="right">
-                        <a href="#about" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300 '>
-                                    <BsPerson size={20} />
+                        <a href="#about" className={navBarButton}>
+                            <BsPerson size={20} />
                         </a>
                     </Tooltip>
                     <Tooltip content="Skills" placement="right">
-                        <a href="#skills" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300 '>
-                                    <AiOutlineProject size={20} className='rotate-180'/>
+                        <a href="#skills" className={navBarButton}>
+                            <AiOutlineProject size={20} className='rotate-180'/>
                         </a>
                     </Tooltip>
                     <Tooltip content="Portfolio" placement="right">
-                        <a href="#portfolio" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300 '>
-                                    <GrProjects size={20} />
+                        <a href="#portfolio" className={navBarButton}>
+                            <GrProjects size={20} />
                         </a>
                     </Tooltip>
                     <Tooltip content="Contact Me" placement="right">
-                        <a href="#contact" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300 '>
-                                    <AiOutlineMail size={20} />
+                        <a href="#contact" className={navBarButton}>
+                            <AiOutlineMail size={20} />
                         </a>
                     </Tooltip>
-                    {/* <a href="#home" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300 '>
-                                <AiOutlineHome size={20} />
-                    </a>
-                    <a href="#about" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300 '>
-                                <BsPerson size={20} />
-                    </a>
-                    <a href="#skills" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300 '>
-                                <AiOutlineProject size={20} />
-                    </a>
-                    <a href="#portfolio" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300 '>
-                                <GrProjects size={20} />
-                    </a>
-                    <a href="#contact" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300 '>
-                                <AiOutlineMail size={20} />
-                    </a> */}
                 </div>
             </div>
         </div>
